baa49a1cbada3ce12dcb06dcc2cb1b286e4d9487
Kamlesh Saini <88968064+oVo-HxBots@users.noreply.github.com>
2023-12-09 17:09:44 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -23,28 +23,40 @@
 # edit this file with your veriable if you'r deploy bot via locally | vps
 
 class Config(object):
-
-    # get a token from @BotFather
-    BOT_TOKEN = ""
-
+    WEBHOOK = os.environ.get("BOT_TOKEN", False)
+    # Get a token from @BotFather
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    # The Telegram API things
+    API_ID = int(os.environ.get("API_ID", 12345))
+    API_HASH = os.environ.get("API_HASH")
     # Get these values from my.telegram.org
-    API_ID = 1234567
-    API_HASH = ""
+    # Array to store users who are authorized to use the bot
 
-    # No need to change
+    # File /video download location
     DOWNLOAD_LOCATION = "./DOWNLOADS"
-    ADL_BOT_RQ = {}
-    CHUNK_SIZE = 128
+
+    MEGA_EMAIL = os.environ.get("MEGA_EMAIL", "None")
+    # If deploying on vps edit the above value as example := Mega_email = "Your-Mega_email-inside-inverted-commas."
+
+    # This is not necessary! Enter your mega password only if you have a mega.nz account with pro/business features.
+    MEGA_PASSWORD = os.environ.get("MEGA_PASSWORD", "None")
+    # If deploying on vps edit the above value as example := Mega_password = "Your-Mega_password-inside-inverted-commas."
+    # Telegram maximum file upload size
     TG_MAX_FILE_SIZE = 4194304000
-    HTTP_PROXY = ""
+
+    # Chunk size that should be used with requests
+    CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
+    # Proxy for accessing youtube-dl in GeoRestricted Areas
+    # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
+    HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
+
+    # Set timeout for subprcess
     PROCESS_MAX_TIMEOUT = 3700
 
-    # TG Ids
-    LOG_CHANNEL = -1007119867241
-    OWNER_ID = 1288398723
-
-    # bot username without @
-    BOT_USERNAME = "AdvanceUrlUploaderBot"
-
-    # auth users
-    AUTH_USERS = [OWNER_ID]
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -100))
+    OWNER_ID = int(os.environ.get("OWNER_ID", "12356"))
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
+    ADL_BOT_RQ = {}
+    AUTH_USERS = list({int(x)
+                      for x in os.environ.get("AUTH_USERS", "0").split()})
+    AUTH_USERS.append(OWNER_ID)
